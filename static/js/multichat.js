1038316860eae3e517b5b0c93ac8016870d2b27e
nirgal <znirgal@gmail.com>
2018-02-13 10:44:08 +0000

    swapped to vis.js, dynamic graphs ftw!

--- static/js/multichat.js
+++ static/js/multichat.js
@@ -30,27 +30,34 @@ function addToConversation(who, msgType, dataString) {
     }
     
     //vis.js
-    addNode(data.messageId, content);
+    addNode(data.messageId, content, nodeColor);
+
+    
 
     //adds message node connected to author
-    elements.push(
-        {
-            group: "nodes", data: {id: data.messageId, label: content},
-            style: {
-                'background-color': nodeColor
-            }
-        },
-        {group: "edges", data: {id: data.author + data.messageId, source: data.messageId, target: data.author}}
-    );
+    //vis.js
+    addEdge(data.author + data.messageId, data.messageId, data.author)
+    
+    // elements.push(
+    //     {
+    //         group: "nodes", data: {id: data.messageId, label: content},
+    //         style: {
+    //             'background-color': nodeColor
+    //         }
+    //     },
+    //     {group: "edges", data: {id: data.author + data.messageId, source: data.messageId, target: data.author}}
+    // );
 
     //links to parents message if not null
     if (data.parentMessageId !== null) {
-        elements.push(
-            {
-                group: "edges",
-                data: {id: who + '_' + data.parentMessageId, source: data.messageId, target: data.parentMessageId}
-            }
-        )
+        addEdge(who + '_' + data.parentMessageId, data.messageId, data.parentMessageId)
+        
+        // elements.push(
+        //     {
+        //         group: "edges",
+        //         data: {id: who + '_' + data.parentMessageId, source: data.messageId, target: data.parentMessageId}
+        //     }
+        // )
     }
 
     //resets graph
@@ -88,18 +95,20 @@ function convertListToButtons(roomName, occupants, isPrimary) {
         });
         
         //vis.js
-        // if (nodes!== undefined){
-        //     addNode(easyrtc.idToName(selfEasyrtcid), easyrtc.idToName(selfEasyrtcid))
-        // }
+       
+            addNode(easyrtc.idToName(selfEasyrtcid), "Me", '#C70039')
+        
+        
+    
         
 
     for (var easyrtcid in occupants) {
         
         //vis.js
-        // if (nodes !== undefined) {
-        //     addNode(easyrtc.idToName(easyrtcid), easyrtc.idToName(easyrtcid))
-        // }
+      
+            addNode(easyrtc.idToName(easyrtcid), easyrtc.idToName(easyrtcid), '#2ecc71')
         
+    
 
         //add all other users in users var
         users.push(
@@ -199,22 +208,15 @@ function getParentMessageId() {
 //Tests with vis.js
 var nodes;
 var edges;
+var network;
 $(function(){
     nodes = new vis.DataSet([
-    {id: 1, label: 'Node 1'}
-    // {id: 2, label: 'Node 2'},
-    // {id: 3, label: 'Node 3'},
-    // {id: 4, label: 'Node 4'},
-    // {id: 5, label: 'Node 5'}
+ 
   ]);
 
   // create an array with edges
   edges = new vis.DataSet([
-    // {from: 1, to: 3},
-    // {from: 1, to: 2},
-    // {from: 2, to: 4},
-    // {from: 2, to: 5},
-    // {from: 3, to: 3}
+  
   ]);
 
   // create a network
@@ -224,16 +226,26 @@ $(function(){
     edges: edges
   };
   var options = {};
-  var network = new vis.Network(container, data, options);
+  network = new vis.Network(container, data, options);
+  
+  // adds event listener on nodes to get answered message's id
+  network.on( 'click', function(properties) {
+    var ids = properties.nodes;
+    var clickedNodes = nodes.get(ids);
+    parentMessageId = clickedNodes[0].id;
+    });
+  
+  
 })
 
 //functions
 //adds a node
-function addNode(id, label) {
+function addNode(id, label, style) {
             try {
                 nodes.add({
                     id: id,
-                    label: label
+                    label: label,
+                    color: style
                 });
             }
             catch (err) {
@@ -293,177 +305,6 @@ function updateNode(id, label) {
                 alert(err);
             }
         }
-
-
-
-
-
-
-
-//list of users
-var users = [];
-// list of graph elements
-var elements = [];
-
-
-function generateGraph() {
-    var cy = cytoscape({
-        //container: document.getElementById('cy'), // container to render in
-
-        elements: elements.concat(users),
-
-        style: [ // the stylesheet for the graph
-            {
-                selector: 'node',
-                style: {
-                    'background-color': '#666',
-                    'color': '#888',
-                    'label': 'data(label)'
-
-                }
-            },
-
-            {
-                selector: 'edge',
-                style: {
-                    'width': 3,
-                    'line-color': '#ccc',
-                    'target-arrow-color': '#ccc',
-                    'target-arrow-shape': 'triangle'
-                }
-            },
-
-            {
-                selector: '.clickedNode',
-                style: {
-                    'background-color': 'brown',
-                    'shape': 'rectangle'
-                }
-
-            }
-        ],
-
-        layout: {
-            name: 'cose',
-
-            // Called on `layoutready`
-            ready: function () {
-            },
-
-            // Called on `layoutstop`
-            stop: function () {
-            },
-
-            // Whether to animate while running the layout
-            // true : Animate continuously as the layout is running
-            // false : Just show the end result
-            // 'end' : Animate with the end result, from the initial positions to the end positions
-            animate: true,
-
-            // Easing of the animation for animate:'end'
-            animationEasing: undefined,
-
-            // The duration of the animation for animate:'end'
-            animationDuration: undefined,
-
-            // A function that determines whether the node should be animated
-            // All nodes animated by default on animate enabled
-            // Non-animated nodes are positioned immediately when the layout starts
-            animateFilter: function (node, i) {
-                return true;
-            },
-
-
-            // The layout animates only after this many milliseconds for animate:true
-            // (prevents flashing on fast runs)
-            animationThreshold: 250,
-
-            // Number of iterations between consecutive screen positions update
-            // (0 -> only updated on the end)
-            refresh: 20,
-
-            // Whether to fit the network view after when done
-            fit: true,
-
-            // Padding on fit
-            padding: 30,
-
-            // Constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-            boundingBox: undefined,
-
-            // Excludes the label when calculating node bounding boxes for the layout algorithm
-            nodeDimensionsIncludeLabels: false,
-
-            // Randomize the initial positions of the nodes (true) or use existing positions (false)
-            randomize: false,
-
-            // Extra spacing between components in non-compound graphs
-            componentSpacing: 40,
-
-            // Node repulsion (non overlapping) multiplier
-            nodeRepulsion: function (node) {
-                return 2048;
-            },
-
-            // Node repulsion (overlapping) multiplier
-            nodeOverlap: 4,
-
-            // Ideal edge (non nested) length
-            idealEdgeLength: function (edge) {
-                return 32;
-            },
-
-            // Divisor to compute edge forces
-            edgeElasticity: function (edge) {
-                return 32;
-            },
-
-            // Nesting factor (multiplier) to compute ideal edge length for nested edges
-            nestingFactor: 1.2,
-
-            // Gravity force (constant)
-            gravity: 1,
-
-            // Maximum number of iterations to perform
-            numIter: 1000,
-
-            // Initial temperature (maximum node displacement)
-            initialTemp: 1000,
-
-            // Cooling factor (how the temperature is reduced between consecutive iterations
-            coolingFactor: 0.99,
-
-            // Lower temperature threshold (below this point the layout will end)
-            minTemp: 1.0,
-
-            // Pass a reference to weaver to use threads for calculations
-            weaver: false
-        }
-
-
-    })
-    // .style().selector(":active")
-    // .css({
-    //     "overlay-color": "black",
-    //     "overlay-padding": 10,
-    //     "overlay-opacity": 0.25 // and others… if the dev wants
-    // });
-
-
-    cy.on('click', 'node', function (evt) {
-        var node = evt.target;
-
-        cy.$('.' + node.id()).addClass('.clickedNode');
-
-        parentMessageId = node.id();
-
-
-        // sendStuffWS(node.id());
-
-
-    });
-
-}
 
 
   
